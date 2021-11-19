869ef72882d737241b60f5e3115bf5f8a1c6a73c
isabellad101 <isabella.drewitt@students.plymouth.ac.uk>
2021-11-19 12:02:26 +0000

    edits

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -23,7 +23,7 @@ int main()
         leds = 0;
 
         //Beep
-        alarm.playTone("A", Buzzer::HIGHER_OCTAVE);
+       alarm.playTone("A", Buzzer::HIGHER_OCTAVE);
         wait_us(250000);
         alarm.rest();
 
@@ -40,123 +40,13 @@ int main()
         // For full marks, debounce the switches and use flow control structures and arrays to avoid deep nesting of code
 
         // ***** MODIFY THE CODE BELOW HERE *****
-     BusIn buttons((USER_BUTTON),(BTN1_PIN),(BTN2_PIN),(BTN3_PIN),(BTN4_PIN));
-     buttons[2].mode(PinMode::PullDown);
-     buttons[3].mode(PinMode::PullDown);
-     int btn = buttons; 
-        
-          switch(btn){
-            case 3:
-            leds=4;
-            break;
-
-            case 16:
-            leds=2;
-            break;
-
-            case 8:
-            leds=1;
-            break;
-
-            case 6:
-            leds=6;
-            break;
-
-            default:
-            leds=0;
-            break;
-          }
-        
-
-        wait_us(100000);
-
-
-
+     
+   
     
-
         // ***** MODIFY THE CODE ABOVE HERE *****
     }
 }
 
-
-  
-
-
-//int sequence[4]{0,0,0,0};
-  //  int total;
-    ////debounce
- //   wait_us(10000);
-   // while(buttons == 31) {
- //   }
- //   wait_us(10000);
- //   //bt1 and bt2
- //   while (buttons == 0) {
- //       if (buttons == 3){
- //           sequence[0]=1;
- //       }
- //       
- //   }
- //   //debounce
-  //  wait_us(10000); 
- //   while(buttons == 31){
- //   }
- //   wait_us(10000);
- //   //bt5
- //   while (buttons == 0){
- //       if (buttons == 16){
- //       sequence[1]=1;
- //       }
- //   }
- //   //debounce
- //   wait_us(10000);
- //   while(buttons == 31){
- //   }
- //   wait_us(10000);
- //   //bt4
- //   while (buttons == 0){
- //       if (buttons == 8){
- //           sequence[2]=1;
- //       }
-        
- //   }
-    //debounce
- //   wait_us(10000);
- //   while(buttons == 31){
- //   }
- //   wait_us(10000);
- //   //bt2 and bt3 
- //   while (buttons == 0){
- //       if (buttons == 6){
- //       sequence[3]=1;
- //       }
- //   } 
-    //debounce
- //   wait_us(10000);
- //   while(buttons == 31){
- //   }
- //   wait_us(10000);
-    //checking only 4 inputs 
- //   for(int i=0;i<=3;i++){
- //       total+=sequence[i];
- //   }
-    // if 4 inputs flash green led 3 times 
- //   if(total==4){
- //       for(int x=0;x<3;x++){
- //       leds[2]=1;
- //       wait_us(500000);
- //       leds[2]=0;
- //       wait_us(500000);
- //       }
-       
- //   }
-    // else play buzzer and turn on red led for 5 seconds
- //   else {
- //   leds[0]=1;
- //   alarm.playTone("A", Buzzer::HIGHER_OCTAVE);
- //   wait_us(5000000);
- //   alarm.rest();
-
-//  }
 
        
 
